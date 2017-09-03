f4a87a83af25be4e987098144eb30063d1d25ad7
zhengshuxin <zhengshuxin@gmail.com>
2017-09-03 18:08:32 +0800

    add master_threads_echod demo for test acl::master_threads service template.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-all: fiber thread thread_mbox fiber_echod http_request redis
+all: fiber thread thread_mbox fiber_echod http_request redis master_threads_echod
 clean cl:
-	rm -f fiber thread thread_mbox fiber_echod http_request redis
+	rm -f fiber thread thread_mbox fiber_echod http_request redis master_threads
 fiber: fiber.cpp
 	g++ fiber.cpp -lfiber_cpp -lfiber -lacl_all -lz -ldl -lpthread -o fiber
 thread: thread.cpp
@@ -13,4 +13,5 @@ http_request: http_request.cpp
 	g++ http_request.cpp -lacl_all -ldl -lz -lpthread -o http_request
 redis: redis.cpp
 	g++ redis.cpp -lacl_all -ldl -lz -lpthread -o redis
-
+master_threads_echod: master_threads_echod.cpp
+	g++ master_threads_echod.cpp -lacl_all -ldl -lz -lpthread -o master_threads_echod
---
+++ master_threads_echod.cpp
@@ -0,0 +1,77 @@
+#include <acl-lib/acl_cpp/lib_acl.hpp>
+#include <getopt.h>
+
+class master_service : public acl::master_threads
+{
+public:
+	master_service(void) {}
+	~master_service(void) {}
+
+private:
+	// @override
+	bool thread_on_accept(acl::socket_stream* conn)
+	{
+		printf("accept from %s\r\n", conn->get_peer());
+		conn->set_rw_timeout(30);
+		return true;
+	}
+
+	// @override
+	bool thread_on_read(acl::socket_stream* conn)
+	{
+		acl::string buf;
+		if (conn->gets(buf) == false)
+			return false;
+		else if (conn->puts(buf) == false)
+			return false;
+		return true;
+	}
+
+	// @override
+	bool thread_on_timeout(acl::socket_stream* conn)
+	{
+		printf("read timeout from %s\r\n", conn->get_peer());
+		return false;
+	}
+
+	// @override
+	void thread_on_close(acl::socket_stream* conn)
+	{
+		printf("close from %s\r\n", conn->get_peer());
+	}
+
+	// @override
+	void proc_on_init(void)
+	{
+		printf("process init now\r\n");
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	acl::string addrs("127.0.0.1:9001, 9002");
+	bool daemon_mode = false;
+	int ch;
+
+	while ((ch = getopt(argc, argv, "s:D")) > 0) {
+		switch (ch) {
+		case 's':
+			addrs = optarg;
+			break;
+		case 'D':
+			daemon_mode = true;
+			break;
+		default:
+			break;
+		}
+	}
+	master_service ms;
+	if (daemon_mode)
+		ms.run_daemon(argc, argv);
+	else
+	{
+		printf("run_alone mode, addrs: %s\r\n", addrs.c_str());
+		ms.run_alone(addrs, NULL, 0, 100);
+	}
+	return 0;
+}
