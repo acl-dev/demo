da9d6f1d5eb039444a5b715d6bb8d9cad37da705
zsx <zsx@cloud-dev-test-dev012-bjdx11.qiyi.virtual>
2019-03-14 15:21:53 +0800

    add connect demo

--- connect.cpp
+++ connect.cpp
@@ -1,4 +1,6 @@
 #include <getopt.h>
+#include <unistd.h>
+#include <acl-lib/acl/lib_acl.h>
 #include <acl-lib/acl_cpp/lib_acl.hpp>
 #include <acl-lib/fiber/libfiber.hpp>
 
@@ -16,6 +18,11 @@ public:
 		peer_ = &peer;
 	}
 
+	void unset_peer(void)
+	{
+		peer_ = NULL;
+	}
+
 	void wait(void)
 	{
 		(void) box_.pop();
@@ -31,14 +38,16 @@ protected:
 			if (ret == -1) {
 				break;
 			}
-
 			if (out_.write(buf, ret) == -1) {
 				break;
 			}
 		}
 
 		if (peer_) {
-			peer_->kill();
+			fiber_transfer* peer = peer_;
+			peer_ = NULL;
+			peer->unset_peer();
+			peer->kill();
 		}
 
 		box_.push(NULL);
@@ -51,22 +60,46 @@ private:
 	fiber_transfer* peer_;
 };
 
+class fiber_proxy : public acl::fiber
+{
+public:
+	fiber_proxy(acl::socket_stream& conn) : conn_(conn) {}
+	~fiber_proxy(void) {}
+
+protected:
+	// @override
+	void run(void)
+	{
+		acl::stdin_stream  in;
+		acl::stdout_stream out;
+
+		ACL_VSTREAM* stream = in.get_vstream();
+		acl_non_blocking(ACL_VSTREAM_SOCK(stream), ACL_NON_BLOCKING);
+
+		fiber_transfer fiber_local(in, conn_);
+		fiber_transfer fiber_peer(conn_, out);
+
+		fiber_local.set_peer(fiber_peer);
+		fiber_peer.set_peer(fiber_local);
+
+		fiber_local.start();
+		fiber_peer.start();
+
+		fiber_local.wait();
+		fiber_peer.wait();
+		acl::fiber::schedule_stop();
+	}
+
+private:
+	acl::socket_stream& conn_;
+};
+
 static void transfer(acl::socket_stream& conn)
 {
-	acl::stdin_stream  in;
-	acl::stdout_stream out;
+	fiber_proxy fb(conn);
+	fb.start();
 
-	fiber_transfer fiber_local(in, conn);
-	fiber_transfer fiber_peer(conn, out);
-
-	fiber_local.set_peer(fiber_peer);
-	fiber_peer.set_peer(fiber_local);
-
-	fiber_local.start();
-	fiber_peer.start();
-
-	fiber_local.wait();
-	fiber_peer.wait();
+	acl::fiber::schedule();
 }
 
 static void usage(const char* procname)
@@ -103,9 +136,32 @@ int main(int argc, char* argv[])
 	acl::socket_stream conn;
 
 	if (conn.open(proxy_addr, 0, 0) == false) {
-		acl::stdout_stream out;
-		out.format("connect proxy(%s) error %s\r\n",
+		printf("connect proxy(%s) error %s\r\n",
 			proxy_addr.c_str(), acl::last_serror());
+		return 1;
+	}
+
+#if 0
+	acl::string buf;
+	buf.format("CONNECT %s HTTP/1.1\r\n\r\n", remote_addr.c_str());
+	if (conn.write(buf) == -1) {
+		printf("write to %s error\r\n", remote_addr.c_str());
+		return 1;
+	}
+#endif
+
+	acl::http_request req(&conn);
+	req.request_header().set_method(acl::HTTP_METHOD_CONNECT)
+		.set_url(remote_addr)
+		.set_host(remote_addr);
+	if (req.request(NULL, 0) == false) {
+		printf("send connect request failed\r\n");
+		return 1;
+	}
+
+	int status = req.http_status();
+	if (status != 200) {
+		printf("invalid status=%d\r\n", status);
 		return 1;
 	}
 
