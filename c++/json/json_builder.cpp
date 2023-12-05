b7875eee941b0427a095cb925e9e58353ee8cbfd
shuxin 　　zheng <zhengshuxin@gmail.com>
2023-12-05 15:31:36 +0800

    Test json demo

--- c++/json/json_builder.cpp
+++ c++/json/json_builder.cpp
@@ -6,9 +6,9 @@ static void test_build1() {
 	acl::json json;
 	acl::json_node& root = json.get_root();
 	acl::json_node& a = root.add_array(true);
-	a.add_text("name", "zsx")
-		.add_number("age", 100)
-		.add_bool("man", true);
+	a.add_child(false, true).add_text("name", "zsx");
+	a.add_child(false, true).add_number("age", 100);
+	a.add_child(false, true).add_bool("man", true);
 
 	acl::string buf;
 	json.to_string(&buf);
@@ -19,9 +19,22 @@ static void test_build2() {
 	acl::json json;
 	acl::json_node& root = json.get_root();
 	acl::json_node& a = root.add_array(true);
-	a.add_child(json.create_node("name", "zsx"))
-		.add_child(json.create_node("age", (long long) 100))
-		.add_child(json.create_node("man", true));
+	a.add_child(false, true).add_text("name", "zsx").get_parent()
+		.add_child(false, true).add_number("age", 100).get_parent()
+		.add_child(false, true).add_bool("man", true);
+
+	acl::string buf;
+	json.to_string(&buf);
+	printf("%s\r\n", buf.c_str());
+}
+
+static void test_build3() {
+	acl::json json;
+	acl::json_node& root = json.get_root();
+	acl::json_node& a = root.add_array(true);
+	a.add_child(json.create_node().add_text("name", "zsx"))
+		.add_child(json.create_node().add_number("age", (long long) 100))
+		.add_child(json.create_node().add_bool("man", true));
 
 	acl::string buf;
 	json.to_string(&buf);
@@ -31,5 +44,6 @@ static void test_build2() {
 int main() {
 	test_build1();
 	test_build2();
+	test_build3();
 	return 0;
 }
