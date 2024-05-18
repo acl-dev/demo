628ef59f5e337e18d76771885d135b8b605405d8
zhengshuxin <zhengshuxin@gmail.com>
2024-05-18 23:35:56 +0800

    Test fiber demo.

--- c++1x/fiber/fiber_echod.cpp
+++ c++1x/fiber/fiber_echod.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <acl-lib/acl_cpp/lib_acl.hpp>
+#include <acl-lib/fiber/libfiber.h>
 #include <acl-lib/fiber/fiber.hpp>
 #include <acl-lib/fiber/go_fiber.hpp>
 
@@ -13,13 +14,13 @@ int main(void) {
 	}
 	printf("listen %s ok\r\n", addr.c_str());
 
-	go[&] {
+	auto fb = go[&] {
 		while (true) {
 			acl::socket_stream* conn = ss.accept();
 			if (conn == NULL) {
 				break;
 			}
-			go[=] {
+			auto fb = go[=] {
 				char buf[8192];
 				while (true) {
 					int ret = conn->read(buf, sizeof(buf), false);
@@ -32,8 +33,10 @@ int main(void) {
 				}
 				delete conn;
 			};
+            printf(">>create echo fiber-%d\r\n", acl_fiber_id(fb));
 		}
 	};
+    printf(">>>server fiber-%d\r\n", acl_fiber_id(fb));
 
 	acl::fiber::schedule();	// start fiber schedule
 	return 0;
