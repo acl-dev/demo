2f0f7d811c0d84e1640a926bb0a7d26a2026dbb0
shuxin 　　zheng <zhengshuxin@gmail.com>
2025-01-03 17:45:38 +0800

    Add fiber pool demo.

---
+++ c++1x/fiber/fiber_pool.cpp
@@ -0,0 +1,164 @@
+#include <unistd.h>
+#include <sys/time.h>
+#include <cstdlib>
+#include <cstdio>
+#include <memory>
+#include <atomic>
+#include <thread>
+
+#include <acl-lib/acl_cpp/lib_acl.hpp>
+#include <acl-lib/fiber/libfiber.h>
+#include <acl-lib/fiber/libfiber.hpp>
+#include <acl-lib/fiber/go_fiber.hpp>
+
+#include "fiber_pool.h"
+
+static void add(acl::wait_group& wg, std::atomic_long& result, int i) {
+    result += i;;
+    printf("Thread-%ld, fiber-%d: add i=%d, result=%ld\n",
+        acl::thread::self(), acl::fiber::self(), i, result.load());
+    wg.done();
+}
+
+static void dec(acl::wait_group& wg, std::atomic_long& result, int i) {
+    result -= i;
+    printf("Thread-%ld, fiber-%d: dec i=%d, result=%ld\n",
+        acl::thread::self(), acl::fiber::self(), i, result.load());
+    wg.done();
+}
+
+static void say(acl::wait_group& wg, std::string data) {
+    printf("Thread-%ld, fiber-%d: say %s\n",
+        acl::thread::self(), acl::fiber::self(), data.c_str());
+    wg.done();
+}
+
+static void fmt_print(acl::wait_group& wg, const char* fmt, ...) {
+    char buf[256];
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(buf, sizeof(buf), fmt, ap);
+    va_end(ap);
+
+    printf("Thread-%ld, fiber-%d print: %s\r\n",
+        acl::thread::self(), acl::fiber::self(), buf);
+    wg.done();
+}
+
+static void usage(const char *procname) {
+    printf("usage: %s -h [help] -c fibers_count -t timeout\r\n", procname);
+}
+
+int main(int argc, char *argv[]) {
+    int ch, nfiber = 10, buf = 500, timeout = -1;
+
+    while ((ch = getopt(argc, argv, "hc:t:")) > 0) {
+        switch (ch) {
+            case 'h':
+                usage(argv[0]);
+                return 0;
+            case 'c':
+                nfiber = atoi(optarg);
+                break;
+            case 't':
+                timeout = atoi(optarg);
+                break;
+            default:
+                usage(argv[0]);
+                return 1;
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+
+    std::atomic_long result(0);
+
+    std::shared_ptr<fiber_pool> fibers(new fiber_pool(buf, nfiber, timeout, true));
+    acl::wait_group wg;
+
+    go[&wg, fibers] {
+        wg.wait();
+        fibers->stop();
+    };
+
+    //////////////////////////////////////////////////////////////////////////
+    // Execute in the currecnt thread.
+
+    for (int i = 0; i < 10; i++) {
+        fibers->exec(add, std::ref(wg), std::ref(result), i);
+        wg.add(1);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        fibers->exec(dec, std::ref(wg), std::ref(result), i);
+        wg.add(1);
+    }
+
+    fibers->exec(say, std::ref(wg), "hello");
+    wg.add(1);
+
+    const char* s = "zsxxsz";
+    fibers->exec(fmt_print, std::ref(wg), "You're welcome, %s!", s);
+    wg.add(1);
+
+    //////////////////////////////////////////////////////////////////////////
+
+    std::thread([&wg, &result] {
+        for (int i = 0; i < 100; i++) {
+            result++;
+            ::usleep(1000);
+        }
+
+        wg.done();
+    }).detach();
+    wg.add(1);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Execute in the current thread's fibers and put by another thread.
+
+    std::thread([&wg, &result, fibers] {
+        for (int i = 0; i < 10; i++) {
+            fibers->exec(add, std::ref(wg), std::ref(result), i);
+            wg.add(1);
+        }
+
+        wg.done();
+    }).detach();
+    wg.add(1);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Execute in the fibers of another thread.
+
+    std::thread([&wg, &result, buf, nfiber, timeout] {
+        std::shared_ptr<fiber_pool> fbs(new fiber_pool(buf, nfiber, timeout));
+        acl::wait_group wg2;
+
+        go[&wg2, fbs] {
+            wg2.wait();
+            fbs->stop();
+        };
+
+        for (int i = 0; i < 10; i++) {
+            fbs->exec(add, std::ref(wg2), std::ref(result), i);
+            wg2.add(1);
+        }
+
+        acl::fiber::schedule();
+        wg.done();
+    }).detach();
+    wg.add(1);
+
+    //////////////////////////////////////////////////////////////////////////
+
+    struct timeval begin;
+    gettimeofday(&begin, nullptr);
+
+    acl::fiber::schedule();
+
+    struct timeval end;
+    gettimeofday(&end, nullptr);
+    double tc = acl::stamp_sub(end, begin);
+
+    printf("The result is %ld, time cost: %.2f ms\r\n", result.load(), tc);
+    return 0;
+}
---
+++ c++1x/fiber/fiber_pool.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <functional>
+#include <future>
+
+using task_fn = std::function<void()>;
+using box_ptr = std::shared_ptr<acl::box2<task_fn>>;
+
+class fiber_pool {
+public:
+    fiber_pool(int buf, int concurrency, int milliseconds, bool thr = false)
+    : milliseconds_(milliseconds)
+    {
+        for (int i = 0; i < concurrency; i++) {
+            std::shared_ptr<acl::box2<task_fn>> box;
+
+            if (thr) {
+                box = std::make_shared<acl::fiber_tbox2<task_fn>>();
+            } else {
+                box = std::make_shared<acl::fiber_sbox2<task_fn>>(buf);
+            }
+
+            boxes_.push_back(box);
+
+            wg_.add(1);
+            auto fb = go[this, box] {
+                fiber_run(box);
+            };
+
+            fibers_.push_back(fb);
+        }
+    }
+
+    ~fiber_pool() = default;
+
+    template<class Fn, class ...Args>
+    auto exec(Fn&& fn, Args&&... args)
+        -> std::future<typename std::result_of<Fn(Args...)>::type> {
+        using return_type = typename std::result_of<Fn(Args...)>::type;
+        auto task = std::make_shared<std::packaged_task<return_type()>>(
+            std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...));
+        auto res = task->get_future();
+        auto obj = [task] { (*task)(); };
+        boxes_[next_++ % boxes_.size()]->push(obj, true);
+        return res;
+    }
+
+    void stop() {
+        for (auto fb : fibers_) {
+            acl_fiber_kill(fb);
+        }
+
+        wg_.wait();
+    }
+
+private:
+    acl::wait_group wg_;
+    int milliseconds_;
+    size_t next_ = 0;
+    std::vector<box_ptr> boxes_;
+    std::vector<ACL_FIBER*> fibers_;
+
+    void fiber_run(const std::shared_ptr<acl::box2<task_fn>>& box) {
+        while (true) {
+            task_fn t;
+            if (box->pop(t, milliseconds_)) {
+                t();
+            } else if (acl::fiber::self_killed()) {
+                break;
+            }
+        }
+
+        wg_.done();
+    }
+};
